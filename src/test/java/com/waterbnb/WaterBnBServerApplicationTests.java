06ee35fce9cabe8245540328323285beba01f235
jimmytran <jt963369@gmail.com>
2019-02-12 20:00:31 -0500

    maven updates

---
+++ src/test/java/com/waterbnb/WaterBnBServerApplicationTests.java
@@ -0,0 +1,36 @@
+package com.waterbnb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WaterBnBServerApplicationTests {
+
+//	private WebDriver webDriver;
+
+//	@Test
+//	public void invokeBrowser() {
+//		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
+//		webDriver = new ChromeDriver();
+//		webDriver.manage().deleteAllCookies();
+//		webDriver.manage().window().maximize();
+//		// Used to synchronize code and wait for code to finish
+//		webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//		webDriver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+//
+//		webDriver.get("http://localhost:4200");
+//	}
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
